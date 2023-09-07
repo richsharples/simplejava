0dea7eb0e47be384a888348f0c0eb3df133f0c5a
Rich Sharples <rich.sharples@gmail.com>
2023-09-07 14:37:33 -0400

    test

--- src/test/java/com/cloudbees/simplejava/AppTest.java
+++ src/test/java/com/cloudbees/simplejava/AppTest.java
@@ -4,9 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-/**
- *
- */
+
 import java.util.List;
 import java.util.ArrayList;
 
